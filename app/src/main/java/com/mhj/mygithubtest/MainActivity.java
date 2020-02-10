7937d1b2ffa7934442f55f3a077c4fdd37502a79
koreadydtkd <dydtkddl2@nate.com>
2020-02-10 10:45:23 +0900

    Initial commit

---
+++ app/src/main/java/com/mhj/mygithubtest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mhj.mygithubtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //시발 문희준새끼
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
